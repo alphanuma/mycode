bed355fae9d8f8798f0d16b164fb70c67699c378
Rock <alphanuma@gmail.com>
2019-04-02 17:30:52 -0400

    Scraping data from API and listing information from Data

--- iss/ride_iss.py
+++ iss/ride_iss.py
@@ -12,10 +12,17 @@ helmet = groundctrl.read()
 ## decode json to python data structure
 helmetson = json.loads(helmet.decode('utf-8'))
 
-## display our pythonic data
-print("\n\nConverted python data")
-print(helmetson)
-
-print("\n\nPeople in Space: ", helmetson['number'])
 people = helmetson['people']
-print(people)
+
+## display our pythonic data
+#print("\n\nConverted python data")
+#print(helmetson)
+
+#prints the people in space at ISS
+print("People in Space: ", helmetson['number'])
+#pull data from the library by name by person
+for person in helmetson["people"]:
+   print("{} on the ISS" .format(person['name']) )
+
+#people = helmetson['people']
+#print(people)
